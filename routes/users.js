70b970751b43df934d32f95b95fb31d2e9394be9
Tim Graves <tim.graves@timgraves.info>
2021-10-31 23:31:52 -0400

    add missing users routes and middleware to git

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,7 @@
 const express = require('express'),
       router  = express.Router(),
-      User    = require('../models/user');
-
-async function getUser(req, res, next) {
-    let user;
-    try {
-        user = await User.findById(req.params.id);
-        if (user === null) {
-            return res.status(404).json({ message: 'Cannot find user' });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-
-    res.user = user;
-    next();
-}
+      User    = require('../models/user')
+      middleware = require('../middleware');
 
 // get all users
 router.get('/', async (req, res) => {
@@ -28,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 // get a single user
-router.get('/:id', getUser, (req, res) => {
+router.get('/:id', middleware.getUser, (req, res) => {
     // res.send('You\'re getting a single user');
     // res.send(res.user.name);
     res.send(res.user.name);
@@ -49,7 +35,7 @@ router.post('/', async (req, res) => {
 });
 
 // updating a user
-router.patch('/:id', getUser, async (req, res) => {
+router.patch('/:id', middleware.getUser, async (req, res) => {
     if (req.body.name != null) {
         res.user.name = req.body.name;
     }
@@ -62,7 +48,7 @@ router.patch('/:id', getUser, async (req, res) => {
  });
 
 // deleting a user
-router.delete('/:id', getUser, async (req, res) => {
+router.delete('/:id', middleware.getUser, async (req, res) => {
     try {
         await res.user.remove();
         res.json({ message: "User removed" });
